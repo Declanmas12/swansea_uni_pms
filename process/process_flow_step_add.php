1e86618e4de6e35bbb82047ba47f523b3fc404b1
Declanmas12 <154251350+Declanmas12@users.noreply.github.com>
2026-03-10 15:16:04 +0000

    Refactor process_flow_step_add.php for clarity

--- process/process_flow_step_add.php
+++ process/process_flow_step_add.php
@@ -28,29 +28,4 @@ $insert->execute([$flowId, $stepId, $position]);
 
 $pdo->commit();
 
-echo "OK";<?php
-require "../config/database.php";
-
-$flowId = $_POST['process_flow_id'];
-$stepId = $_POST['process_step_id'];
-
-/* Find next order */
-$stmt = $pdo->prepare("
-    SELECT COALESCE(MAX(step_order),0)+1
-    FROM process_flow_steps
-    WHERE process_flow_id=?
-");
-$stmt->execute([$flowId]);
-$order = $stmt->fetchColumn();
-
-/* Insert */
-$stmt = $pdo->prepare("
-    INSERT INTO process_flow_steps
-    (process_flow_id, process_step_id, step_order)
-    VALUES (?, ?, ?)
-");
-
-$stmt->execute([$flowId, $stepId, $order]);
-
 echo "OK";
-
