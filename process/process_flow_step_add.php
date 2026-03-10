b3f39f8a9186889d91f3ae25b4c98fd84a3f1871
Declanmas12 <154251350+Declanmas12@users.noreply.github.com>
2026-03-10 14:59:49 +0000

    Implement step insertion at specified position
    Added functionality to insert a new step at a specified position, shifting existing steps down.

--- process/process_flow_step_add.php
+++ process/process_flow_step_add.php
@@ -3,6 +3,36 @@ require "../config/database.php";
 
 $flowId = $_POST['process_flow_id'];
 $stepId = $_POST['process_step_id'];
+$position = $_POST['position'] ?? 1;
+
+$pdo->beginTransaction();
+
+/* Shift steps down */
+$shift = $pdo->prepare("
+    UPDATE process_flow_steps
+    SET step_order = step_order + 1
+    WHERE process_flow_id = ?
+    AND step_order >= ?
+");
+
+$shift->execute([$flowId, $position]);
+
+/* Insert new step */
+$insert = $pdo->prepare("
+    INSERT INTO process_flow_steps
+    (process_flow_id, process_step_id, step_order)
+    VALUES (?, ?, ?)
+");
+
+$insert->execute([$flowId, $stepId, $position]);
+
+$pdo->commit();
+
+echo "OK";<?php
+require "../config/database.php";
+
+$flowId = $_POST['process_flow_id'];
+$stepId = $_POST['process_step_id'];
 
 /* Find next order */
 $stmt = $pdo->prepare("
@@ -23,3 +53,4 @@ $stmt = $pdo->prepare("
 $stmt->execute([$flowId, $stepId, $order]);
 
 echo "OK";
+
